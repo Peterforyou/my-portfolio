1afb927444a6e0b9cedc3f3016ff567a36f67aa3
Peterforyou <110077295+Peterforyou@users.noreply.github.com>
2022-10-23 03:52:12 +0100

    style.css edited

---
+++ app.js
@@ -0,0 +1,12 @@
+
+// function checke () {
+//     let harmburger = document.querySelector('.checkme').checked = true;
+//     let text = document.querySelector('.navigation nav ul li a').onclick = () => {
+//         alert('hi!')
+//     }
+//     text()
+// }
+// function me() {
+//     alert('me')
+// }
+// me();
