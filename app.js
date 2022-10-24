6f9dce8b9b5d52f382815555034be3e535d2ac2e
Peterforyou <110077295+Peterforyou@users.noreply.github.com>
2022-10-24 23:02:54 +0100

    style.css styled

--- app.js
+++ app.js
@@ -10,3 +10,32 @@
 //     alert('me')
 // }
 // me();
+
+// let section2 = document.querySelector('.section-2');
+// let what = document.querySelector('.what');
+// let section3 = document.querySelector('.section-3');
+// let section4 = document.querySelector('.section-4');
+// let section5 = document.querySelector('.section-5');
+// let section6 = document.querySelector('.section-6');
+// let toggler = document.querySelector('.toggler');
+
+// toggler.addEventListener('click', () => {
+//     section2.style.backgroundColor = '#fff'
+//     toggler.style.transform = 'translateX(1.3rem)'
+//     toggler.style.transition = 'all .5s'
+
+
+// })
+
+// function backToLight () {
+//     if (toggler.style.transform = 'translateX(1.3rem)') {
+//         toggler.style.transform = 'translateX(-2.6rem)'
+//     } else {
+
+//     }
+// }
+
+// function me () {
+//     let element = document.body;
+//     element.classList.toggle ('dark-mode')
+// }
